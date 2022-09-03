8dd488e4db2ef1b7922cb1002bb309d5000b6890
akcelluler <112778787+akcelluler@users.noreply.github.com>
2022-09-04 02:46:13 +0700

    Create menu-pengaturan.sh

---
+++ menu/menu-pengaturan.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+clear
+echo -e "\e[36m╒════════════════════════════════════════════╕\033[0m"
+echo -e " \E[0;41;36m                 INFO SERVER                \E[0m"
+echo -e "\e[36m╘════════════════════════════════════════════╛\033[0m"
+uphours=`uptime -p | awk '{print $2,$3}' | cut -d , -f1`
+upminutes=`uptime -p | awk '{print $4,$5}' | cut -d , -f1`
+uptimecek=`uptime -p | awk '{print $6,$7}' | cut -d , -f1`
+cekup=`uptime -p | grep -ow "day"`
+IPVPS=$(curl -s ipinfo.io/ip )
+ISPVPS=$( curl -s ipinfo.io/org )
+#clear
+if [ "$cekup" = "day" ]; then
+echo -e "System Uptime   :  $uphours $upminutes $uptimecek"
+else
+echo -e "System Uptime   :  $uphours $upminutes"
+fi
+echo -e "IP-VPS          :  $IPVPS"
+echo -e "ISP-VPS         :  $ISPVPS"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                       ┃ Script By Akcell ┃                      ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                           085730123218                          ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"
+echo "╔═════════════════════════════════════════════════════════════════╗"
+echo "║                     ┃ XRAY XMENU PENGATURAN ┃                   ║" 
+echo "╚═════════════════════════════════════════════════════════════════╝"  
+echo "║ 1. Pengaturan                                                   ║"
+echo "║ 2. limitspeed                                                   ║"
+echo "║ 3. Cek Port                                                     ║"
+echo "║ 4.                                                              ║"
+echo "║ 5.                                                              ║"
+echo "║ 6. Menu Utama                                                   ║"
+echo "║ 7. Exit                                                         ║"
+echo "╚═════════════════════════════════════════════════════════════════╝" 
+read -p "Select From Options [ 1 - 7 ] : " menu
+echo -e ""
+case $menu in 
+1)
+pengaturan
+;;
+2)
+limitspeed
+;;
+3)
+cek-port
+;;
+4)
+pengaturan4
+;;
+5)
+pengaturan5
+;;
+6)
+clear
+menu
+;;
+7)
+clear
+exit
+;;
+*)
+clear
+menu-pengaturan
+;;
+esac

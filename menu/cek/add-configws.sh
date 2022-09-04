b464a3d354232a0fc400391b2fa9f5f9a57727c6
akcelluler <112778787+akcelluler@users.noreply.github.com>
2022-09-04 07:58:55 +0700

    Update add-configws.sh

--- menu/cek/add-configws.sh
+++ menu/cek/add-configws.sh
@@ -46,7 +46,7 @@ NUMBER_OF_CLIENTS=$(grep -c -E "^##### " "/etc/xray/configmultiakun.json")
 	echo " Press CTRL+C to return"
 	echo " ==============================="
 	echo -e	"  NO ${GREEN}USER   ${RED}EXPIRED ${BLUE}Net${NC}"
-        grep -E "^##### " "/etc/xray/configmultiakun.json" | cut -d ' ' -f 2,3,6,7 | nl -s ') '
+        grep -E "^##### " "/etc/xray/configmultiakun.json" | cut -d ' ' -f 2-3 | nl -s ') '
 	until [[ ${CLIENT_NUMBER} -ge 1 && ${CLIENT_NUMBER} -le ${NUMBER_OF_CLIENTS} ]]; do
 		if [[ ${CLIENT_NUMBER} == '1' ]]; then
 			read -rp "Select one client [1]: " CLIENT_NUMBER
